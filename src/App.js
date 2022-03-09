5fa88e79283b8e15fc2405fee07cee8c60dfd886
nazmul367 <najmulrizvy@gmail.com>
2022-03-09 11:18:51 +0600

    basic redux implementation

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+const App = () => {
+
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
+  console.log(counter)
+
+  const increment = () => {
+    dispatch({ type: 'INC' })
+  }
+  const decrement = () => {
+    dispatch({ type: 'DEC' })
+  }
+
+  const addBY = () => {
+    dispatch({ type: 'ADD', payload: 20 })
+  }
+
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button type='button' onClick={increment}>Increment</button>
+      <button type='button' onClick={decrement}>Decrement</button>
+      <button type='button' onClick={addBY}>Add By</button>
+    </div>
+  );
+}
+
+export default App;
