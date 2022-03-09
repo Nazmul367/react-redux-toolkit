32c9ce51accc81a20b3e4cb15d679ba9eb11d7e9
nazmul367 <najmulrizvy@gmail.com>
2022-03-09 11:42:29 +0600

    remove console

--- src/store/index.js
+++ src/store/index.js
@@ -23,27 +23,3 @@ const store = configureStore({
 })
 
 export default store
-
-
-// import { createStore } from "redux";
-
-// const reducerFn = (state = { counter: 10 }, action) => {
-
-//     switch (action.type) {
-//         case 'INC':
-//             return { counter: state.counter + 1 }
-
-//         case 'DEC':
-//             return { counter: state.counter - 1 }
-
-//         case 'ADD':
-//             return {counter: state.counter + action.payload}
-
-//         default:
-//             return state
-//     }
-// }
-
-// const store = createStore(reducerFn)
-
-// export default store
